1f7716596867c7fb23a10c4a6d50472f003cdf9c
zhouzhangqi <1978892854@qq.com>
2024-07-19 00:47:10 +0800

    feature: support tls

--- crm/src/client.rs
+++ crm/src/client.rs
@@ -2,15 +2,26 @@ use crm::{
     pb::{crm_client::CrmClient, WelcomeRequestBuilder},
     AppConfig,
 };
-use tonic::Request;
+use tonic::{
+    transport::{Certificate, Channel, ClientTlsConfig},
+    Request,
+};
 use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let config = AppConfig::load().expect("Failed to load config");
-    let addr = format!("http://[::1]:{}", config.server.port);
+    let addr = Box::leak(Box::new(format!("https://[::1]:{}", config.server.port)));
     println!("{}", addr);
-    let mut client = CrmClient::connect(addr).await?;
+    let pem = include_str!("../../fixtures/rootCA.pem");
+    let tls = ClientTlsConfig::new()
+        .ca_certificate(Certificate::from_pem(pem))
+        .domain_name("localhost");
+    let channel = Channel::from_static(addr)
+        .tls_config(tls)?
+        .connect()
+        .await?;
+    let mut client = CrmClient::new(channel);
     let req = WelcomeRequestBuilder::default()
         .id(Uuid::new_v4().to_string())
         .interval(9)
--- crm/src/server.rs
+++ crm/src/server.rs
@@ -1,6 +1,8 @@
+use std::mem;
+
 use crm::{AppConfig, CrmService};
 
-use tonic::transport::Server;
+use tonic::transport::{Identity, Server, ServerTlsConfig};
 use tracing::{info, level_filters::LevelFilter};
 use tracing_subscriber::{
     fmt::Layer, layer::SubscriberExt as _, util::SubscriberInitExt as _, Layer as _,
@@ -11,12 +13,22 @@ async fn main() -> anyhow::Result<()> {
     let layer = Layer::new().with_filter(LevelFilter::INFO);
     tracing_subscriber::registry().with(layer).init();
 
-    let config = AppConfig::load()?;
+    let mut config = AppConfig::load()?;
+    let tls = mem::take(&mut config.server.tls);
     let addr = config.server.port;
     let addr = format!("[::1]:{}", addr).parse()?;
     let scv = CrmService::try_new(config).await?.into_server();
 
     info!("Crm Server listening on {}", addr);
-    Server::builder().add_service(scv).serve(addr).await?;
+    if let Some(tls) = tls {
+        let identity = Identity::from_pem(tls.cert, tls.key);
+        Server::builder()
+            .tls_config(ServerTlsConfig::new().identity(identity))?
+            .add_service(scv)
+            .serve(addr)
+            .await?;
+    } else {
+        Server::builder().add_service(scv).serve(addr).await?;
+    }
     Ok(())
 }
